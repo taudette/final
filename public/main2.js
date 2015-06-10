d77e622b4caa100c0e596425dff8e1865fee0ff5
Tyler Audette <tyleraudette@yahoo.com>
2015-06-10 11:46:43 -0600

    back to before

--- public/main2.js
+++ public/main2.js
@@ -1,5 +1,12 @@
 var requestApp = angular.module('requestApp', ['ngResource', 'ngRoute', 'uiGmapgoogle-maps', 'gm.datepickerMultiSelect']);
 
+requestApp.config(function($routeProvider) {
+    $routeProvider
+    .otherwise('/');
+    //load page using otherwise to add #/
+    
+});
+
 requestApp.factory('climbRequests', function($resource){
 	var model = $resource('/api/:id', {id: '@_id'});
 	return{
@@ -8,10 +15,23 @@ requestApp.factory('climbRequests', function($resource){
 		items: model.query()
 	};
 });
-requestApp.controller('requestController', function($scope, $http, climbRequests){
+
+requestApp.controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+  function ($scope, $location, $anchorScroll) {
+    $scope.gotoMap = function() {
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash('map');
+
+      // call $anchorScroll()
+      $anchorScroll();
+    };
+  }]);
+requestApp.controller('requestController', function($scope, $http, $location, $anchorScroll, climbRequests){
 	
 	$scope.items = climbRequests.items;
 	$scope.markerList = [{}];
+
 
 	var showMarkers = function(){
 		//sending request to server side endpoint (api)
@@ -39,6 +59,7 @@ requestApp.controller('requestController', function($scope, $http, climbRequests
 
 
 	showMarkers();
+
 	$scope.addItem = function(){
 		var newRequest = new climbRequests.model($scope.newItem);
 		newRequest.$save(function(savedItem){
@@ -59,29 +80,16 @@ requestApp.controller('requestController', function($scope, $http, climbRequests
 	        			})
 	        			.error(function(data) {
 	        				console.log(data);
-	        			});        		
-	        		
-		     	}
-		     	var marker = new new google.maps.Marker({
-	        			id:savedItem._id, 
-	        			latitude: $scope.latitude, 
-	        			longitude: $scope.longitude,
-	        			map: $scope.map,
-	        			title:'string of test',
-	        			labelContent:'please work',
-	        			labelClass:'labels'
-	        		});
-					// var iw1 = new google.maps.InfoWindow({
-					// 	content: "Home For Sale"
-					// });
-					google.maps.event.addListener(marker, "click", function () { 
-						console.log('click');
-						// iw1.open(map, this); 
-					});
-	        		
-	        	
+	        			});
+	        			var marker = new google.maps.Marker({
+	        				id:savedItem._id, latitude: $scope.latitude, longitude: $scope.longitude
+	        			});
+
+
+
 					$scope.markerList.push(marker);	
-					
+				
+		     	}
 			});	
 		});
 	$scope.newItem = {};		
@@ -96,8 +104,7 @@ requestApp.controller('requestController', function($scope, $http, climbRequests
 
 	//map stuff
 	
-	$scope.map = { center: { latitude: 39.8282, longitude: -98.5 }, zoom: 3, options:{ styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#e66e3b"},{"visibility":"on"}]}]} };
-
+	$scope.map = { center: { latitude: 39.8282, longitude: -98.5 }, zoom: 3, options:{ styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"color":"#675a4b"}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry","stylers":[{"color":"#ffebc5"},{"lightness":"-10"}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#675a4b"}]},{"featureType":"administrative.country","elementType":"labels.text.fill","stylers":[{"color":"#b70046"}]},{"featureType":"administrative.province","elementType":"geometry.fill","stylers":[{"visibility":"off"}]},{"featureType":"administrative.province","elementType":"geometry.stroke","stylers":[{"color":"#675a4b"},{"weight":"0.50"}]},{"featureType":"administrative.province","elementType":"labels.text.fill","stylers":[{"color":"#675a4b"}]},{"featureType":"administrative.locality","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"administrative.locality","elementType":"labels.text.fill","stylers":[{"color":"#ff850a"}]},{"featureType":"administrative.neighborhood","elementType":"geometry","stylers":[{"visibility":"on"}]},{"featureType":"administrative.neighborhood","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"saturation":"-71"},{"lightness":"-2"},{"color":"#ffebc5"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#70bfaf"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#675a4c"}]},{"featureType":"road.highway","elementType":"labels.text.fill","stylers":[{"color":"#675a4b"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.arterial","elementType":"labels.text.fill","stylers":[{"color":"#675a4b"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7ccff0"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#cfeae4"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#109579"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"visibility":"off"}]}]} };
 
 });
 
